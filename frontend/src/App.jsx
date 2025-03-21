a941b33aaf4937e4756de6c33f7ccdf0d1a1f913
Sapandeep <110763985+sapandeep31@users.noreply.github.com>
2025-03-21 22:02:21 +0530

    basicEmployee

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,18 @@
-import './App.css'
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AdminPage from './pages/AdminPage/AdminPage';
 import SignUp from './pages/SignUp/SignUp';
 import ChildDashboard from './pages/ChildDashboard/ChildDashboard';
+import EmployeeDashboard from './pages/EmployeeDashboard/EmployeeDashboard';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/SignUp" element={<SignUp/>}/>
+        <Route path="/SignUp" element={<SignUp />} />
         <Route path="/admin" element={<AdminPage />} />
         <Route path="/child-dashboard/:childName" element={<ChildDashboard />} />
+        <Route path="/employee-dashboard/:educatorID" element={<EmployeeDashboard />} />
       </Routes>
     </Router>
   );
